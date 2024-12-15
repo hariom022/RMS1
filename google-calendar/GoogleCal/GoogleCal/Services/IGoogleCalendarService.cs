0c49bf62a3caa32f4ed99471526f09a838d9136d
amolrogye <34906522+amolrogye@users.noreply.github.com>
2024-12-15 18:21:45 +0530

    Delete google-calendar/GoogleCal directory

--- google-calendar/GoogleCal/GoogleCal/Services/IGoogleCalendarService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Google.Apis.Calendar.v3.Data;
-
-namespace GoogleCal.Services
-{
-    public interface IGoogleCalendarService
-    {
-        Task<Event> CreateEvent(Event request, CancellationToken cancellationToken);
-    }
-}
